a12c8bacf13f5c33d0f33f3b13e05dfc3fb3c49d
st3fanV <st3fanvogt@googlemail.com>
2016-05-30 13:50:51 +0200

    Gaze3DFixGUI Project and Documentation

---
+++ Gaze3DFixGUI/Project/Gaze3DFixGUI/Gaze3DFixGUI/Model/FixationFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaze3DFixGUI.Model
+{
+    class FixationFile
+    {
+        public String filename = "";
+
+        public List<FixationData> list_FixationData = new List<FixationData>();
+
+        public FixationFile ()
+        {
+
+        }
+
+        public void addFixationData(FixationData fixationdata)
+        {
+            list_FixationData.Add(fixationdata);
+        }
+    }
+}
---
+++ Gaze3DFixGUI/Project/Gaze3DFixGUI/Gaze3DFixGUI/Model/GazeFile3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaze3DFixGUI.Model
+{
+    class GazeFile3D
+    {
+        public String filename = "";
+
+        public List<GazeData3D> list_GazeData3D = new List<GazeData3D>();
+
+        public GazeFile3D ()
+        {
+
+        }
+
+        public void addGazeData3D(GazeData3D gazedata3d)
+        {
+            list_GazeData3D.Add(gazedata3d);
+        }
+    }
+}
